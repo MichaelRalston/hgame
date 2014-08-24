b6188f80bb7f0c2bc98699f6e7e95612e76e698d
Michael Ralston <michael.ralston@gmail.com>
2014-08-24 15:28:22 -0700

    Remove debugging seq.

--- Test/Tests.hs
+++ Test/Tests.hs
@@ -9,7 +9,7 @@ import Control.Concurrent.MVar (modifyMVar, withMVar, newMVar)
 import Control.Monad (void)
 import Control.Concurrent (forkIO)
 
-test = forkIO $ void $ do
+test = forkIO $ void $ forkIO $ void $ do
 	a <- newMVar 5
 	r <- modifyMVar a (\a' ->
 		return $ (a'+1, Network.WebSockets.Text $ encode $ object ["screen" .= (a' :: Int), "thing" .= (6 :: Int)]))
