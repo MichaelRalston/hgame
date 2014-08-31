5b7100834112bdf010a2dcbdda9fdc3fe3d2d76a
Michael Ralston <michael.ralston@gmail.com>
2014-08-31 16:14:08 -0700

    makeCardTable is now WithMemory instead of IO.

--- Rules/CardTable/CardTable.hs
+++ Rules/CardTable/CardTable.hs
@@ -5,7 +5,6 @@ module Rules.CardTable.CardTable
 import Rules.CardTable.Types
 import Engine.Types
 import System.Random (StdGen)
-import Control.Concurrent (newMVar)
 
 renderer :: CardTableState -> PlayerIndex -> Screen CardZone CardEntity
 renderer = undefined
@@ -13,10 +12,10 @@ renderer = undefined
 inputHandler :: InputHandler CardTableState CardEntity CardZone
 inputHandler = undefined
 	
-makeCardTable :: StdGen -> IO Game
+makeCardTable :: StdGen -> WithMemory Game
 makeCardTable generator = do
 	let deck = [CECard suit rank | suit <- ["red", "blue", "purple", "yellow"], rank <- [1..15]]
-	tableState <- newMVar $ CTS
+	tableState <- alloc $ CTS
 		{ hands = [[],[]]
 		, decks = [deck,[]]
 		, tables = [[],[]]
