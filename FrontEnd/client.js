2d522ecfec28482f0965a30b63bafd6d11011d5f
Michael Ralston <michael.ralston@gmail.com>
2014-09-20 19:14:09 -0700

    debug info.

--- FrontEnd/client.js
+++ FrontEnd/client.js
@@ -145,6 +145,7 @@ function makeZone(zoneId, displayData) {
 			$zone.dialog(
 				{ autoOpen: false
 				});
+			console.log("Number of linked entities", $('#entity-' + displayData.entity).length);
 			$('#entity-' + displayData.entity).click(function() {
 				$zone.dialog("open");
 			});
