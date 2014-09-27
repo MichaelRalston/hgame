3ca7af780ffc533001d696668a8af2b4b876d7e8
Michael Ralston <michael.ralston@gmail.com>
2014-09-26 21:16:59 -0700

    More debugging.

--- FrontEnd/client.js
+++ FrontEnd/client.js
@@ -182,6 +182,8 @@ function getZone(zoneData) {
 					if (!$zone.is(ui.draggable.parent())) {
 						var id = ui.draggable.attr('id');
 						console.log("Dropping ",id,"on",zoneId);
+						console.log(event);
+						console.log(ui);
 						if (id) {
 							sendMsg({'action':'drag','zone':zoneId,'entity':id.slice(7)});
 						}
